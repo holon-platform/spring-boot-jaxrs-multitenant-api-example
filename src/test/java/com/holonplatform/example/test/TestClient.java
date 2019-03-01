fec5d655c63848ee71fcb2d772b1958838304d97
Riccardo Righi <rr9379@gmail.com>
2019-03-01 11:21:10 +0100

    Update to Holon Platform version 5.2.2

--- src/test/java/com/holonplatform/example/test/TestClient.java
+++ src/test/java/com/holonplatform/example/test/TestClient.java
@@ -10,9 +10,9 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.holonplatform.core.property.PropertyBox;
